24ecc0d87a171c9fa38bee0051ee0c5d716b284d
Lulu-1121 <hd162@duke.edu>
2024-10-30 05:01:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-def create_dataframe():
+def create_heroes_dataframe():
     data = {
         'Heroes': ['Kailin', 'Kaiserui', 'Shakete', 'Adela', 'Luna', 'Siqiangke', 'Taiz', 'Zhanshu'],
         'Skill 1': ['Houqin', 'Zhaohunshu', 'Jingongshu', 'Waijiaoshu', 'Zhihuishu', 'Zhaohunshu', 'Fangyushu', 'Jianshu'],
@@ -10,18 +10,17 @@ def create_dataframe():
         'Hero Score': [9.5, 10.0, 9.5, 9.5, 9.5, 3.0, 9.0, 8.0],
         'Race Score': [8.5, 9.5, 7.5, 9.0, 9.5, 9.5, 8.5, 7.0]
     }
-    
-    df = pd.DataFrame(data)
-    return df
+    heroes_df = pd.DataFrame(data)
+    return heroes_df
 
-def modify_dataframe(df):
-    df['Skill Score'] = np.array([9.5, 9, 9, 8.5, 8, 7, 9, 9])
+def modify_heroes_dataframe(heroes_df):
+    heroes_df['Skill Score'] = np.array([9.5, 9, 9, 8.5, 8, 7, 9, 9])
     new_row = ['Gengna', 'Houqin', 'Zhanshu', 'Bilei', 9.5, 7.5, 9.5]
-    df.loc[8] = new_row
-    df.loc[0, 'Race Score'] = 8.5
-    return df
+    heroes_df.loc[8] = new_row
+    heroes_df.loc[0, 'Race Score'] = 8.5
+    return heroes_df
 
 if __name__ == "__main__":
-    df = create_dataframe()
-    df = modify_dataframe(df)
-    print(df)
+    heroes_dataframe = create_heroes_dataframe()
+    modified_heroes_dataframe = modify_heroes_dataframe(heroes_dataframe)
+    print(modified_heroes_dataframe)
