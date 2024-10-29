925915b9c850a5770ea4a76d3a30a4bb4cbc6dce
Lulu-1121 <hd162@duke.edu>
2024-10-30 05:02:28 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,18 +1,18 @@
 import unittest
 import numpy as np
 import pandas as pd
-from main import create_dataframe, modify_dataframe
+from main import create_heroes_dataframe, modify_heroes_dataframe
 
 class TestMainFunctions(unittest.TestCase):
 
-    def test_create_dataframe(self):
-        df = create_dataframe()
+    def test_create_heroes_dataframe(self):
+        df = create_heroes_dataframe()
         self.assertEqual(len(df), 8)
         self.assertIn('Heroes', df.columns)
 
-    def test_modify_dataframe(self):
-        df = create_dataframe()
-        modified_df = modify_dataframe(df)
+    def test_modify_heroes_dataframe(self):
+        df = create_heroes_dataframe()
+        modified_df = modify_heroes_dataframe(df)
         self.assertEqual(len(modified_df), 9)
         self.assertIn('Skill Score', modified_df.columns)
         self.assertEqual(modified_df.loc[0, 'Race Score'], 8.5)
